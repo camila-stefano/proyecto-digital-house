90113464aaceebdfc7a59e850e07d5d1b10263bb
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2024-06-09 20:45:26 -0300

    add React course files

---
+++ project-react/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Fetch</h1>
+      <div className="card">
+        <p>Lorem kjskasjdjskdkjhfa</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
