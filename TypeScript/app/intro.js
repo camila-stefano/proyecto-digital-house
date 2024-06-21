a254bc646d3f07c6485b745dc762db60a3937708
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2024-06-21 17:52:29 -0300

    add Tipos de Datos TS

--- TypeScript/app/intro.js
+++ TypeScript/app/intro.js
@@ -1,3 +1,44 @@
 "use strict";
 let saludo = "Hola mundo ts";
-console.log(saludo);
+let nombre = "Juan";
+let edad = 23;
+let esMayor = true;
+let colores = ["rojo", "verde", "azul"];
+let colores2 = ["rojo", "verde", "azul"];
+var DiasSemana;
+(function (DiasSemana) {
+    DiasSemana[DiasSemana["Lunes"] = 0] = "Lunes";
+    DiasSemana[DiasSemana["Martes"] = 1] = "Martes";
+    DiasSemana[DiasSemana["Miercoles"] = 2] = "Miercoles";
+    DiasSemana[DiasSemana["Jueves"] = 3] = "Jueves";
+    DiasSemana[DiasSemana["Viernes"] = 4] = "Viernes";
+    DiasSemana[DiasSemana["Sabado"] = 5] = "Sabado";
+    DiasSemana[DiasSemana["Domingo"] = 6] = "Domingo";
+})(DiasSemana || (DiasSemana = {}));
+let hoy = DiasSemana.Lunes;
+// Funciones con tipos y parametros de retorno
+function saludar(nombre) {
+    return `Hola como estas ${nombre}`;
+}
+console.log(saludar("Cami"));
+function saludar2(nombre) {
+    console.log(`Hola saludo 2 ${nombre}`);
+}
+saludar2("Cami");
+// Clases con propiedades y metodos tipeados:
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    mostrarDetalles() {
+        console.log(`Producto: ${this.nombre}`);
+    }
+}
+let colors = "rojo";
+// Uso de tipos genericos
+function duplicar(valor) {
+    return valor;
+}
+const numeroDuplicado = duplicar(5);
+const textoDuplicado = duplicar("Hola");
--- TypeScript/app/intro.ts
+++ TypeScript/app/intro.ts
@@ -1,3 +1,60 @@
 let saludo = "Hola mundo ts";
 
-console.log(saludo);
+let nombre: string = "Juan";
+
+let edad: number = 23;
+
+let esMayor: boolean = true;
+
+let colores: string[] = ["rojo", "verde", "azul"];
+
+let colores2: Array<string> = ["rojo", "verde", "azul"];
+
+enum DiasSemana {
+  Lunes,
+  Martes,
+  Miercoles,
+  Jueves,
+  Viernes,
+  Sabado,
+  Domingo,
+}
+
+let hoy: DiasSemana = DiasSemana.Lunes;
+
+// Funciones con tipos y parametros de retorno
+
+function saludar(nombre: string): string {
+  return `Hola como estas ${nombre}`;
+}
+
+console.log(saludar("Cami"));
+
+function saludar2(nombre: string): void {
+  console.log(`Hola saludo 2 ${nombre}`);
+}
+
+saludar2("Cami");
+
+// Clases con propiedades y metodos tipeados:
+
+class Producto {
+  constructor(public nombre: string, public precio: number) {}
+  mostrarDetalles(): void {
+    console.log(`Producto: ${this.nombre}`);
+  }
+}
+
+type Color = "rojo" | "verde" | "amarillo";
+
+let colors: Color = "rojo";
+
+// Uso de tipos genericos
+
+function duplicar<T>(valor: T) {
+  return valor;
+}
+
+const numeroDuplicado: number = duplicar(5);
+
+const textoDuplicado: string = duplicar("Hola");
